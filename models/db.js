37539aeead9f1852de659a8ce89755b01e1218ba
Parameswar Mondal <parameswar.mondal@fissionlabs.com>
2022-05-30 23:40:52 +0530

    build(config change): change config file

--- models/db.js
+++ models/db.js
@@ -3,16 +3,16 @@
 const config = require('../config/config');
 const mongoose = require('mongoose');
 
-// mongodb://localhost:27017/magicDB
-
 let username = config.DEFAULT_USERNAME;
 let password = config.DEFAULT_PASSWORD;
 let database = config.DEFAULT_DATABASE;
 let host = config.DEFAULT_HOST;
 let dbPort = config.DEFAULT_DBPORT;
+let environment = config.NODE_ENV;
 
-let connectionUrl = `mongodb://${username}:${password}@${host}:${dbPort}/${database}?ssl=true&retrywrites=false`;
+let connectionUrl = (environment !== 'local') ? `mongodb://${username}:${password}@${host}:${dbPort}/${database}?ssl=true&retrywrites=false` : `mongodb://${host}:${dbPort}/${database}`;
 console.log('connectionUrl ', connectionUrl);
+
 mongoose.connect(connectionUrl, { useNewUrlParser: true }, (err) => {
     if (!err) {
         console.log('MongoDB connection Succeeded!');
